cfc5ed839e2ff50a0596d3f75a71b9959c04b4ce
Mai-GitHubb <119874424+Mai-GitHubb@users.noreply.github.com>
2022-12-22 23:30:48 +0530

    Update PASSWROD GENERATOR.py
    1. Compressed big lines.
    2. Added symbols.

--- PASSWROD GENERATOR.py
+++ PASSWROD GENERATOR.py
@@ -1,9 +1,10 @@
 def random_pass(length, no_num, no_sym):
     import random as r
-    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-         'w', 'x', 'y', 'z']
-    num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    sym = ['!', '@', '#', '$', '%', '^', '&', '*']
+    import string
+    alpha = list(string.ascii_letters)
+    num = [str(i) for i in range(0,10)]
+    sym = ['!', '#', '$', '%', '&', "'", '"', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?',
+           '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
     ranalpha, rannum, ransym = [], [], []
 
 # random alphabets
